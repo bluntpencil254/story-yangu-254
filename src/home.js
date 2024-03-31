dc8618d89732b057b632054052a51ca48c4611d9
bluntpencil254 <makorimike9@gmail.com>
2024-03-31 09:08:10 +0300

    added error catch functionality

--- src/home.js
+++ src/home.js
@@ -2,34 +2,36 @@ import { useState, useEffect } from "react";
 import BlogList from "./bloglist";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(
-        [
-            {title: 'My New website.', body: 'dyudgetwfet dfgetedftdew tdfet', author: 'Wilson Rotich', id: 1},
-            {title: 'Welcome Party!', body: 'dyudgetwfet dfgetedftdew tdfet', author: 'John Mark', id: 2},
-            {title: 'Dancing Monkeys.', body: 'dyudgetwfet dfgetedftdew tdfet', author: 'Michelle Opiyo', id: 3},
-            {title: 'My birthday Treat.', body: 'dyudgetwfet dfgetedftdew tdfet', author: 'Wilson Rotich', id: 4},
-            {title: 'A holiday to remember.', body: 'dyudgetwfet dfgetedftdew tdfet', author: 'Wilson Rotich', id: 5}
-        ]);
-
-        const [name, setName] = useState('Wilson Rotich');
-
-        const handleDelete = (id) => {
-            const newBlogs = blogs.filter(blog => blog.id !== id);
-            setBlogs(newBlogs);
-        }
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null)
 
         useEffect(() => {
-            console.log('use effect run');
-            console.log(name);
-        }, [name]);
+          setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    if (!res.ok) {
+                       throw Error('could not fetch the data for the resource'); 
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setBlogs(data);
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch(err =>{
+                    setIsPending(false);
+                    setError(err.message);
+                } )
+          }, 2000);
+        }, []);
 
     return ( 
         <div className="home">
-           <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
-           
-           <BlogList blogs={blogs.filter((blog) =>blog.author === "Wilson Rotich")} title="Blogs by Wilson"/>
-          <button onClick={() => setName('Wilson Ngeno')}>Change Name</button>
-          <p>{ name }</p>
+            { error && <div>{ error }</div>}
+            {isPending && <div>Loading...</div> }
+           {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
      );
 }
