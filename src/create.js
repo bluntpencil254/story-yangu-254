3b2497d9a7276625cbaeffd17ab670952bb1980f
bluntpencil254 <makorimike9@gmail.com>
2024-04-06 12:22:40 +0300

    just customised new blog page

--- src/create.js
+++ src/create.js
@@ -1,7 +1,44 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState(' ');
+    const [body, setBody] = useState(' ');
+    const [author, setAuthor] = useState('Wilson Rotich');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = {title, body, author};
+    }
+
     return ( 
         <div className="create">
             <h2>Add a new blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="title">Blog title</label>
+                <input type="text"
+                required
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                />
+                <label htmlFor="title">Blog body</label>
+                <textarea required
+                value={body}
+                onChange={(e)=> setBody(e.target.value)}
+                ></textarea>
+                <label htmlFor="author">Blog author</label>
+                <select 
+                    value={author}
+                onChange={(e)=> setAuthor(e.target.value)}
+
+                >
+                    <option value="Wilson Rotich">Wilson Rotich</option>
+                    <option value="John Mark">John Mark</option>
+                </select>
+                <button>Add Blog</button>
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
+
+            </form>
         </div>
      );
 }
